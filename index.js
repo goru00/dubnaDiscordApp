c271699e80da5ac7c8960426f2ad1ef18f3e4425
goru00 <d-tretyakov-artim2012@yandex.ru>
2021-08-31 20:25:02 +0300

    Аdd message ready bot

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
+require('dotenv').config();
 const { Client, Intents, MessageAttachment } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 const disk = require('ya-disk');
-require('dotenv').config();
-const Discord = require('discord.js');
 const Canvas = require('canvas');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-const API_TOKEN = process.env.TOKEN;
+
+var nowDate = new Date();
+const API_TOKEN_YD = process.env.TOKEN;
+
+client.on('ready', message => {
+  message.channels.cache.get('803286281147908096').send(`Перезапуск бота. Дата и время запуска: ${nowDate}`);
+});
 
 client.on("message", async message => {
   if (message.author.bot) return;
   if (!message.content.startsWith("!")) return;
+
   const commandBody = message.content.slice("!".length);
   const commandArgs = commandBody.split(' ');
   const command = commandArgs.shift().toLowerCase();
+
   if (command === "расписание") {
-    const canvas = Canvas.createCanvas(600, 700);
+    const canvas = Canvas.createCanvas(600, 800);
     const context = canvas.getContext('2d');
     const bg = await Canvas.loadImage(path.resolve('files', 'timetable.jpg'));
     context.drawImage(bg, 0, 0, canvas.width, canvas.height);
-    const atach = new Discord.MessageAttachment(canvas.toBuffer(), 'timetable.png');
+    const atach = new MessageAttachment(canvas.toBuffer(), 'timetable.png');
     console.log("message send");
     message.reply({ files: [atach] });
-  } else if (command === "help" || command === "помощь") {
+
+  } 
+  else if (command === "help" || command === "помощь") {
+
     console.log("message send");
     let filecontent = fs.readFileSync(path.resolve('files', 'help.txt'), "utf8");
     message.reply(filecontent);
-  } else {
+
+  } 
+  else {
+
     message.reply('Сорян, братик, в этой ситуации я не абонент.');
+
   } 
 });
 
